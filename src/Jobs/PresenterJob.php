3128a46e09a9a932db8719c6da0c8a40a710ea0c
Martin Brettschneider <martin.brettschneider@adbros.cz>
2020-04-10 18:45:12 +0200

    PresenterJob

---
+++ src/Jobs/PresenterJob.php
@@ -0,0 +1,96 @@
+<?php declare(strict_types = 1);
+
+namespace Adbros\Worker\Jobs;
+
+use Nette\PhpGenerator\PhpFile;
+use Nette\Utils\Strings;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Exception\InvalidOptionException;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class PresenterJob extends AbstractJob
+{
+
+	public static function getCommandName(): string
+	{
+		return 'worker:presenter';
+	}
+
+	public function configureCommand(Command $command): void
+	{
+		parent::configureCommand($command);
+
+		$command
+			->setDescription('Generate Nette presenter.')
+			->addArgument('name', InputArgument::OPTIONAL, 'Presenter name')
+			->addOption('namespace', 'ns', InputOption::VALUE_OPTIONAL, 'Presenter namespace');
+	}
+
+	public function interact(InputInterface $input, SymfonyStyle $io, Command $command): void
+	{
+		parent::interact($input, $io, $command);
+
+		if ($input->getArgument('name') === null) {
+			$entity = $io->ask('Enter presenter name', null, function (?string $answer): string {
+				if ($answer === null) {
+					throw new InvalidArgumentException('Please, enter presenter name.');
+				}
+
+				return $answer;
+			});
+
+			$input->setArgument('name', $entity);
+		}
+
+		if ($input->getOption('namespace') === null) {
+			$namespace = $io->ask('Enter presenter namespace', $input->getOption('root-namespace') . '\\Presenters', function (?string $answer) use ($input): string {
+				if ($answer === null || !Strings::startsWith($answer, $input->getOption('root-namespace'))) {
+					throw new InvalidOptionException('Presenter namespace must be part of root namespace.');
+				}
+
+				return $answer;
+			});
+
+			$input->setOption('namespace', $namespace);
+		}
+	}
+
+	public function generate(InputInterface $input, SymfonyStyle $io): int
+	{
+		$directory = $input->getOption('root-directory') .
+			$this->namespaceToPath($input->getOption('namespace'), $input->getOption('root-namespace'));
+
+		if (!file_exists($directory)) {
+			mkdir($directory, 0777, true);
+		}
+
+		$filename = $directory . '/' . $input->getArgument('name') . 'Presenter.php';
+
+		if (file_exists($filename)) {
+			throw new RuntimeException(sprintf('File %s already exists!', $filename));
+		}
+
+		$file = new PhpFile();
+
+		if (method_exists($file, 'setStrictTypes')) {
+			$file->setStrictTypes(true);
+		}
+
+		$file
+			->addNamespace($input->getOption('namespace'))
+			->addClass($input->getArgument('name') . 'Presenter')
+			->setExtends('Nette\\Application\\UI\\Presenter');
+
+		file_put_contents($filename, (string) $file);
+
+		$io->success('Done.');
+
+		return 0;
+	}
+
+}
